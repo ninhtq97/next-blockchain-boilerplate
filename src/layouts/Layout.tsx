a0c828862f1c3a0cb2b1050c7ac30370ac9984d4
ninhtq97 <tranquangninh97@gmail.com>
2022-11-23 15:42:22 +0700

    update boilerplate

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,9 @@
 type Props = {} & React.HTMLAttributes<HTMLElement>;
 
 const Layout: React.FC<Props> = ({ className, children }) => {
-  return <div className={`app ${className || ''}`}>{children}</div>;
+  return (
+    <div className={`app${className ? ` ${className}` : ''}`}>{children}</div>
+  );
 };
 
 export default Layout;
